33c5efcbe6380f3b0e409265c7f3ade104d7e243
Adam Grubb <adam.eriksson.grubb@iths.se>
2023-08-23 20:29:58 +0200

    Add view interface and implement in GameController

--- CleanCodeLaboration/Controller/GameController.cs
+++ CleanCodeLaboration/Controller/GameController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CleanCodeLaboration.Model.GameDAO.Interface;
+using CleanCodeLaboration.Model.GameLogic.Interface;
+using CleanCodeLaboration.View.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +11,15 @@ namespace CleanCodeLaboration.Controller
 {
     public class GameController
     {
-        public GameController(IGameContext )
+        private readonly IGameContext gameContext;
+        private readonly IView view;
+
+        public GameController(IGameContext gameContext, IView view)
         {
-                
+            this.gameContext = gameContext;
+            this.view = view;
         }
+
 
     }
 }
