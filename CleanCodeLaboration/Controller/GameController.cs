8f6f424cccebbf443cdf3bb0432297177d24cf2c
Adam Grubb <adam.eriksson.grubb@iths.se>
2023-08-24 08:59:15 +0200

    Finish GameController

--- CleanCodeLaboration/Controller/GameController.cs
+++ CleanCodeLaboration/Controller/GameController.cs
@@ -1,5 +1,7 @@
 ﻿using CleanCodeLaboration.Model.GameDAO.Interface;
 using CleanCodeLaboration.Model.GameLogic.Interface;
+using CleanCodeLaboration.Model.GameLogic.Strategy;
+using CleanCodeLaboration.Model.GameLogic.Strategy.Interface;
 using CleanCodeLaboration.View.Interface;
 using System;
 using System.Collections.Generic;
@@ -19,7 +21,59 @@ namespace CleanCodeLaboration.Controller
             this.gameContext = gameContext;
             this.view = view;
         }
+        public void StartCleanCodeGameLoop()
+        {
+            SetUserName();
+            StartGameLoop();
 
+        }
+        public void SetUserName()
+        {
 
+            string playerNameQuestion = gameContext.GetPlayerNameQuestion();
+            view.GameOutput(playerNameQuestion);
+            string playerName = view.GetUserInput();
+            gameContext.SetPlayerName(playerName);
+        }
+        public void StartGameLoop()
+        {
+            do
+            {
+                GetGameMenu();
+                GetGameLoop();
+            } while (ContinuePlaying());
+
+        }
+        public void GetGameMenu() //Här kommer du implementera meny-klassen. Mest troligt via dependency injection.
+        {
+            //Dummy-metod. DAO sätts inom SetGameStrategy - möjligen via en builder?
+            //Här har du en stänga av funktion också.
+            gameContext.SetGameStrategy(new MooGameStrategy());
+        }
+        public void GetGameLoop()
+        {
+            string gameIntroduction = gameContext.GetGameIntroduction();
+
+            view.GameOutput(gameIntroduction);
+
+            while (gameContext.IsGameActive())
+            {
+                string userGuess = view.GetUserInput();
+                string gameUpdateMessage = gameContext.EvaluateGuess(userGuess);
+                view.GameOutput(gameUpdateMessage);
+            }
+            string highScore = gameContext.GetHighScore();
+            view.GameOutput(highScore);
+
+            string finishedGameMessage = gameContext.GetFinishedGameMessage();
+            view.GameOutput(finishedGameMessage);
+        }
+        private bool ContinuePlaying()
+        {
+            string askIfKeepPlaying = gameContext.GetPlayAgainMessage();
+            view.GameOutput(askIfKeepPlaying);
+            string answer = view.GetUserInput();
+            return gameContext.KeepPlaying(answer);
+        }
     }
 }
