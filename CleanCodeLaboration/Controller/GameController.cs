bfe9b692304ba7d9e4aace4c8574006c0a1e06e9
Adam Grubb <adam.eriksson.grubb@iths.se>
2023-09-02 19:12:48 +0200

    Remove GameLoopLogic in GameController

--- CleanCodeLaboration/Controller/GameController.cs
+++ CleanCodeLaboration/Controller/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using CleanCodeLaboration.Model.GameLogic.Interface;
 using CleanCodeLaboration.Model.GameLogic.Strategy.Interface;
+using CleanCodeLaboration.Model.GameLoop.Interface;
 using CleanCodeLaboration.Model.GameMenu;
 using CleanCodeLaboration.Model.GameMenu.Interface;
 using CleanCodeLaboration.View.Interface;
@@ -8,51 +9,30 @@ namespace CleanCodeLaboration.Controller
 {
     public class GameController
     {
-        private readonly IGameContext gameContext;
-        private readonly IIO iO;
+        private readonly IGameLoop gameLoop;
         private IGameMenu gameMenu;
 
-        public GameController(IGameContext gameContext, IIO iO, IGameMenu gameMenu)
+        public GameController(IGameLoop gameLoop, IGameMenu gameMenu)
         {
-            this.gameContext = gameContext;
-            this.iO = iO;
+            this.gameLoop = gameLoop;
             this.gameMenu = gameMenu;
         }
 
-        public void InitializeGame() //Lite osäker på namnet?
+        public void StartGame() //Lite osäker på namnet?
         {
-            AskForPlayerName();
-            SetUserName();
-            StartGameLoop();
-        }
-
-
-        private void AskForPlayerName()
-        {
-            string playerNameQuestion = gameContext.GetPlayerNameQuestion();
-            iO.GameOutput(playerNameQuestion);
-        }
-        private void SetUserName()
-        {
-
-            string playerName = iO.GetUserInput();
-            gameContext.SetPlayerName(playerName);
-        }
-
-        private void StartGameLoop() //Egentligen borde GetGameLoop kanske heta StartGameLoop.
-        {
-            do 
-            {
-                InitializeGameMenu(); //Dnna borde ha ett namn som betyder att den både ger output och förväntar sig input. Namn som typ StartGameMenu?
-                GetGameLoop(); //Denna borde inte också heta gameLoop? Den här innehåller ju det spelet man valt.
-                GetAskIfContinuePlaying();
-            } while (ShouldContinuePlaying());
+            InitializeGameMenu();
         }
 
         private void InitializeGameMenu() //Denna behöver ett namnbyte.
         {
-            OutputMenu();
-            ChooseGame();
+            do
+            {
+                OutputMenu();
+                ChooseGame();
+                StartGameLoop();
+            } while (KeepPlaying());
+
+
         }
         private void OutputMenu()
         {
@@ -65,69 +45,15 @@ namespace CleanCodeLaboration.Controller
         }
         private void SetGameStrategy(IGameStrategy gameStrategy)
         {
-            gameContext.SetGameStrategy(gameStrategy);
+            gameLoop.SetGameStrategy(gameStrategy);
         }
-
-
-        private void GetGameLoop()
+        private void StartGameLoop()
         {
-            StartNewGame();
-
-            GameIntroduction(); //OutputGameIntroduktion?
-
-            GetCorrectAnswer(); //ShowCorrectAnswer? OutPutCorrectAnswer?
-
-            GetUserGuesses(); //Här ska det framgå mer att det är nån slags guess-loop?
-
-            GetHighScore();
-
-            GetFinishedGameMessage();
-            
+            gameLoop.GetGameLoop();
         }
-        private void StartNewGame() //Start Game?
+        private bool KeepPlaying()
         {
-            gameContext.StartNewGame();
-        }
-        private void GameIntroduction()
-        {
-            string gameIntroduction = gameContext.GetGameIntroduction();
-
-            iO.GameOutput(gameIntroduction);
-        }
-        private void GetCorrectAnswer()
-        {
-            string rightAnswer = gameContext.GetRightAnswer();
-            iO.GameOutput(rightAnswer);
-        }
-        private void GetUserGuesses()
-        {
-            while (gameContext.IsGameActive())
-            {
-                string userGuess = iO.GetUserInput();
-                string gameUpdateMessage = gameContext.CheckPlayerAnswer(userGuess);
-                iO.GameOutput(gameUpdateMessage);
-            }
-        }
-        private void GetHighScore()
-        {
-            string highScore = gameContext.GetHighScore();
-            iO.GameOutput(highScore);
-        }
-        private void GetFinishedGameMessage()
-        {
-            string finishedGameMessage = gameContext.GetFinishedGameMessage();
-            iO.GameOutput(finishedGameMessage);
-        }
-
-        private bool ShouldContinuePlaying() //Hur blir det här, den borde ju lägga nära metoden som använder den, men om den har metoder som använder den hur gör man då?
-        {
-            string answer = iO.GetUserInput();
-            return gameContext.KeepPlaying(answer);
-        }
-        private void GetAskIfContinuePlaying()
-        {
-            string askIfKeepPlaying = gameContext.GetPlayAgainMessage();
-            iO.GameOutput(askIfKeepPlaying);
+           return gameMenu.ContinuePlaying();
         }
     }
 }
