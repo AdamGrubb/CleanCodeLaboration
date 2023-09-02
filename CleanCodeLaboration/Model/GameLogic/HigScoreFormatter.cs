f57fd69a70f575e200035e049f27aa11d56e0ea2
Adam Grubb <adam.eriksson.grubb@iths.se>
2023-09-02 10:24:40 +0200

    Create HighScoreFormatter-class and interface

---
+++ CleanCodeLaboration/Model/GameLogic/HigScoreFormatter.cs
@@ -0,0 +1,17 @@
+﻿using CleanCodeLaboration.Model.GameLogic.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeLaboration.Model.GameLogic
+{
+    public class HigScoreFormatter : IHigScoreFormatter
+    {
+        public string FormatHighScores()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CleanCodeLaboration/Model/GameLogic/Interface/IHigScoreFormatter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeLaboration.Model.GameLogic.Interface
+{
+    public interface IHigScoreFormatter
+    {
+        string FormatHighScores();
+    }
+}
