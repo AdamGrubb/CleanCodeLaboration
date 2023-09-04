dc0e01db65e3b21539143c2a49e5b8c7319e51f6
Adam Grubb <adam.eriksson.grubb@iths.se>
2023-09-04 18:23:14 +0200

    Add comment in IGameStrategy

--- CleanCodeLaboration/Model/GameLogic/Strategy/Interface/IGameStrategy.cs
+++ CleanCodeLaboration/Model/GameLogic/Strategy/Interface/IGameStrategy.cs
@@ -14,7 +14,7 @@ namespace CleanCodeLaboration.Model.GameLogic.Strategy.Interface
         List<IPlayerScore> GetPlayerScores();
         void SetPlayerName(string userName);
         void ActivateGame(); //Fortfarande kanske otydligt vad den gör? För abstrakt? StartNewGame?
-        void SaveGame();
+        void SaveGame(); //Skulle jag kunna stoppa in name här istället då?
         void IncrementGuessCount(); //Denna kanske behöver bytas namn på, kanske borde heta något i stil med incrementNumberOfGuesses?
         bool IsCorrectGuess(string guess);
         void DeactivateGame();
