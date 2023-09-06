d98479b0e641610ae1b097366532000e54c25635
Adam Grubb <adam.eriksson.grubb@iths.se>
2023-09-06 16:50:12 +0200

    Create GameStrategyFactory

---
+++ CleanCodeLaboration/Model/GameStrategyFactory/GameStrategyFactory.cs
@@ -0,0 +1,19 @@
+﻿using CleanCodeLaboration.Model.GameLogic.Strategy.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeLaboration.Model.GameStrategyFactory
+{
+    public abstract class GameStrategyFactory
+    {
+
+        protected abstract IGameStrategy GetStrategy();
+        public IGameStrategy CreateStrategy()
+        {
+            return GetStrategy();
+        }
+    }
+}
